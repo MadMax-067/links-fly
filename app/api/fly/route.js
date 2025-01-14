7cdf2b9a4693c7d4ededbc05685899bf99c903b7
MadMax-067 <gfxnxt@gmail.com>
2025-01-14 14:40:35 +0530

    method change

--- app/api/fly/route.js
+++ app/api/fly/route.js
@@ -1,4 +1,4 @@
-import clientPromise from "@/app/lib/mongodb";
+import { MongoClient } from "mongodb";
 
 // Function to generate a random short string
 function generateFlyurl(length = 8) {
@@ -11,6 +11,13 @@ function generateFlyurl(length = 8) {
 }
 
 export async function POST(req) {
+
+    const uri = process.env.MONGODB_URI; // Your MongoDB URI
+    const options = {};
+
+    const mongo = new MongoClient(uri, options);
+    const clientPromise = mongo.connect();
+
     try {
         // Parse the request body
         const body = await req.json();
