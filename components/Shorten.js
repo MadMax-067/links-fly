d20308f1ed10f3b6bd0e4903443eaca4572d3b9c
MadMax-067 <gfxnxt@gmail.com>
2025-01-15 18:42:44 +0530

    v0 fixes

--- components/Shorten.js
+++ components/Shorten.js
@@ -78,7 +78,7 @@ const Shorten = () => {
                     <span className='flex flex-col px-2 gap-2 items-center md:items-start ' >
                         <label htmlFor="linkInput" className='card-title font-bold text-sm md:text-xl' >{!isFly ? "Paste your long link here" : "Your link is ready to fly!🛫"}</label>
 
-                        {!isFly ? <input value={userURL} onChange={e => { setUserURL(e.target.value) }} id='linkInput' type="url" placeholder='https://example.com/a-long-url' className='linkInput w-full bg-transparent flex-1 p-3 outline-none border border-base-100 rounded-lg hover:border-primary' /> : <button onClick={copyToClipboard} className="btn md:text-xl border border-base-100 rounded-lg hover:border-primary">{flyURL}</button>}
+                        {!isFly ? <input value={userURL} onChange={e => { setUserURL(e.target.value) }} id='linkInput' type="url" placeholder='https://example.com/a-long-url' className='linkInput w-full bg-transparent flex-1 p-3 outline-none border border-base-100 rounded-lg hover:border-primary' /> : <button onClick={copyToClipboard} className="btn w-full md:text-xl border border-base-100 rounded-lg hover:border-primary">{flyURL}</button>}
 
                         {errorMessage && (
                             <div className="text-red-500 text-sm md:text-base mt-2">{errorMessage}</div>
@@ -88,7 +88,7 @@ const Shorten = () => {
                             <div className="text-green-500 text-sm md:text-base mt-2">{isCopied}</div>
                         )}
 
-                        {!isFly ? <button onClick={flyLink} className='btn btn-outline font-bold flex flex-col justify-center items-center mt-2 rounded-2xl w-56 text-base'><img src="/URL-BORDERD.gif" className='w-5' /><span>Fly your link for free</span></button> : <button onClick={flyAgain} className='btn btn-outline w-full font-bold flex flex-col justify-center items-center mt-2 rounded-2xl w-56 text-base'><img src="/URL-BORDERD.gif" className='w-5' /><span>Fly your links again?</span></button>}
+                        {!isFly ? <button onClick={flyLink} className='btn btn-outline font-bold flex flex-col justify-center items-center mt-2 rounded-2xl w-56 text-base'><img src="/URL-BORDERD.gif" className='w-5' /><span>Fly your link for free</span></button> : <button onClick={flyAgain} className='btn btn-outline font-bold flex flex-col justify-center items-center mt-2 rounded-2xl w-56 text-base'><img src="/URL-BORDERD.gif" className='w-5' /><span>Fly your links again?</span></button>}
                     </span>
                 </div>
             </div>
